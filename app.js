df03be721609e9c27b26b27d0bec9b609a69d738
Shinya Aoi <saoi@Shinyas-MacBook-Pro.local>
2019-08-24 12:41:28 +0900

    First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use(bodyParser());
+app.use(morgan());
+
+app.use(express.static('public'));
+
+//reading css files via express server
+app.use("/assets", express.static(path.join(__dirname, "assets")));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+const listener = app.listen(process.env.PORT, function(){
+  console.log('your app is running on port ' + listener.address().port);
+})
+
